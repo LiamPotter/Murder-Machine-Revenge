5868539ee02bf0264556f1f15013688ddf0cd9b8
LiamPotter <liamp62@gmail.com>
2016-10-26 14:48:27 +1000

    Jumpy Jumps
    -Some Jumpy Jump tests

---
+++ Assets/Scripts/Jumpy/JumpScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using TriTools;
+public class JumpScript : MonoBehaviour {
+
+    public AnimationCurve jumpCurve;
+
+    private AnimationCurve finishCurve;
+
+    public bool isJumping;
+
+    public float jumpStrength;
+
+    public float jumpTime;
+
+    public float y;
+
+    private float timer = 0.0f;
+
+    public Vector3 gravityStrength;
+
+    private Rigidbody rBody;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        jumpCurve.postWrapMode = WrapMode.PingPong;
+    }
+
+	void FixedUpdate ()
+    {
+        Physics.gravity = gravityStrength;
+       
+        if (Input.GetKey(KeyCode.Space))
+            isJumping = true;
+        else isJumping = false;
+        if (isJumping)
+        {
+            if (timer <= jumpTime)
+            {
+                y = jumpCurve.Evaluate(Mathf.Clamp((timer/jumpTime)*2,0,2));
+                //Debug.Log(y);
+                timer += Time.deltaTime;
+                //transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, y, transform.position.z),Time.deltaTime*2f);
+                //transform.position = new Vector3(0, y, 0);
+                TriToolHub.AddForce(gameObject, TriToolHub.XYZ.Y, y*jumpStrength, Space.World, ForceMode.Acceleration);
+            }      
+        }
+        if(!isJumping)
+        {
+            timer = 0.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Jumpy/MoveScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using TriTools;
+
+
+public class MoveScript : MonoBehaviour {
+
+    public float moveSpeed;
+
+    public Camera cam;
+
+    private Vector3 moveDirection;
+
+    private Transform childTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        childTransform = GetComponentInChildren<Transform>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        TriToolHub.CreateVector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), moveSpeed, TriToolHub.AxisPlane.XZ, cam.gameObject, out moveDirection);
+        TriToolHub.SmoothLookAtDirection(childTransform.gameObject, moveDirection, 0, Vector3.up, true, 1);
+        TriToolHub.AddForce(gameObject, childTransform.forward * Input.GetAxis("Vertical")*moveSpeed, Space.World, ForceMode.Acceleration);
+	}
+}
